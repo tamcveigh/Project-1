931c6a22960cfaa1c6a24d71967e7beea3b75ab7
HARPWNER <HARPWNER@HARPWNER-PC>
2018-09-18 14:06:42 -0400

    Completed Person class, Heavy work on Employee class.

--- src/Employee.java
+++ src/Employee.java
@@ -15,17 +15,81 @@ public class Employee implements AttributeInterface {
 	private String department;
 	
 	//TODO Finish Employee Class
+	public Employee(Person person, String id, String phone, String division, String years, String department) {
+		this.person = person;
+		this.id = id;
+		this.phone = phone;
+		this.division = division;
+		this.years = years;
+		this.department = department;
+	}
+	public Employee(String first, String last, Person.Status status, String id, String phone, String division, String years, String department) {
+		this.person = new Person(first, last, status);
+		this.id = id;
+		this.phone = phone;
+		this.division = division;
+		this.years = years;
+		this.department = department;
+	}
 	
 	@Override
 	public boolean check(String attribute, String value) {
-		// TODO Auto-generated method stub
+		switch(attribute) {
+		case "first name":
+		case "first":
+			return(this.person.getFirstName().equals(value));
+		case "last name":
+		case "last":
+			return(this.person.getLastName().equals(value));
+		case "marital status":
+		case "status":
+			return(this.person.getMaritalStatus().equals(value));
+		case "id":
+			return(this.id.equals(value));
+		case "phone":
+			return(this.phone.equals(value));
+		case "division":
+			return(this.division.equals(value));
+		case "years":
+			return(this.years.equals(value));
+		case "department":
+			return(this.department.equals(value));
+		}
 		return false;
 	}
 
 	@Override
 	public boolean change(String attribute, String value) {
-		// TODO Auto-generated method stub
-		return false;
+		switch(attribute) {
+		case "first name":
+		case "first":
+			person.setFirstName(value);
+			return true;
+		case "last name":
+		case "last":
+			person.setLastName(value);
+			return true;
+		case "marital status":
+		case "status":
+			return(person.setMaritalStatus(value));
+		case "id":
+			this.id = value;
+			return true;
+		case "phone":
+			this.phone = value;
+			return true;
+		case "division":
+			this.division = value;
+			return true;
+		case "years":
+			this.years = value;
+			return true;
+		case "department":
+			this.department = value;
+			return true;
+		default:
+			return false;
+		}
 	}
 
 	@Override
--- src/Person.java
+++ src/Person.java
@@ -8,10 +8,55 @@ public class Person {
 	/** Marital Status */
 	private Status status;
 	
-	//TODO Write Person Class
-	
-	private enum Status{
-		//TODO Write Status enum
+	public enum Status{
+		Married,
+		Single,
+		Widowed,
+		Divorced;
 	}
 	
+	public Person(String first, String last, Status status) {
+		this.first = first;
+		this.last = last;
+		this.status = status;
+	}
+	
+	public String getFirstName() {
+		return first;
+	}
+	
+	public String getLastName() {
+		return last;
+	}
+	
+	public String getMaritalStatus() {
+		return status.toString();
+	}
+	
+	public void setFirstName(String fName) {
+		this.first = fName;
+	}
+	
+	public void setLastName(String lName) {
+		this.last = lName;
+	}
+	
+	public Boolean setMaritalStatus(String status) {
+		switch(status) {
+		case "Married":
+			this.status = Status.Married;
+			return true;
+		case "Single":
+			this.status = Status.Single;
+			return true;
+		case "Widowed":
+			this.status = Status.Widowed;
+			return true;
+		case "Divorced":
+			this.status = Status.Divorced;
+			return true;
+		default:
+			return false;
+		}
+	}
 }
